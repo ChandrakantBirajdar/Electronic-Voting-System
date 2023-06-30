4ef231860245b362c24800923c8e613a0fb1c1f2
ChandrakantBirajdar <88143599+ChandrakantBirajdar@users.noreply.github.com>
2023-06-30 23:25:54 +0530

    Add files via upload
    Proper Authentication for Administrative and Electoral Officer

---
+++ EVS/EVS/admin.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include "header.h"
+
+void viewRequest()
+{
+
+    struct Request request;
+    char choice[10];
+
+    FILE *requestfile = fopen("VoterIdRequest.csv", "r");
+
+    if (requestfile == NULL)
+    {
+        perror("Error in file opening\n");
+    }
+    else
+    {
+
+        while (fread(&request, sizeof(request), 1, requestfile))
+        {
+
+            printf("User with UseerId %d has requested for VoterId ", request.UserId);
+            printf("Do you want to approve the request? \n");
+            printf("Enter your choice [y/n]: ");
+            scanf("%s", choice);
+            if (strcmp(choice, "y") == 0 || strcmp(choice, "Y") == 0)
+            {
+                FILE *approvedUser = fopen("ApprovedUserFile.csv", "a");
+                strcpy(request.status, "Approved");
+                printf("Request Approved successfully \n");
+                fwrite(&request, sizeof(request), 1, approvedUser);
+                fclose(approvedUser);
+            }
+            if (strcmp(choice, "n") == 0 || strcmp(choice, "N") == 0)
+            {
+                printf("Request denied successfully \n");
+            }
+        }
+
+        fclose(requestfile);
+        requestfile = fopen("VoterIdRequest.csv", "w");
+        fclose(requestfile);
+    }
+}
+
+void adminMenu()
+{
+
+    struct Admin admin;
+
+    char username1[50];
+    char password1[50];
+    int choice = 0;
+    char ch[10];
+
+    strcpy(admin.username, "chandrakant");
+    strcpy(admin.password, "birajdar");
+
+    printf("\nEnter the Username: ");
+    scanf("%s", username1);
+
+    printf("\nEnter the password: ");
+    scanf("%s", password1);
+
+    if (strcmp(admin.username, username1) == 0 && strcmp(admin.password, password1) == 0)
+    {
+
+        while (choice != 2)
+        {
+            printf("\n---------------------------------------------------------|\n");
+            printf("     \033[0;32m  Admin Menu\033[0;32m                      \033[0;36m|\033[0;37m\n");
+            printf("---------------------------------------------------------|\n");
+            printf("Please select an option:  ");
+            printf("\n1.view Request ");
+            printf("\n2.Exit");
+            printf("\n");
+            printf(" Enter The Choice: ");
+
+            // scanf("%d", &choice1);
+
+            scanf("%s", ch);
+            choice = atoi(ch);
+            switch (choice)
+            {
+            case 1:
+                viewRequest();
+                break;
+            case 2:
+                break;
+
+            default:
+                printf("\033[0;31m\n");
+                printf(" Invalid Choice !!!  Please Enter Valid choise !!! \n");
+                printf("\033[0;37m\n");
+            }
+        }
+    }
+    else
+    {
+        printf("\nInvalid username or password");
+    }
+}
